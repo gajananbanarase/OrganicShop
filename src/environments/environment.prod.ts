0e3d0ab21b5f2a8ea2bf92e0e714e1ee1ce61f17
Gajanan Banarase <gbanarase450@gmail.com>
2019-01-22 14:42:51 +0530

    Install firebase and angularfire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+      apiKey: "AIzaSyC-0Yn5eD97LYPocRIYh7TrcRru6zZl1Qw",
+      authDomain: "organicshop-d16b9.firebaseapp.com",
+      databaseURL: "https://organicshop-d16b9.firebaseio.com",
+      projectId: "organicshop-d16b9",
+      storageBucket: "organicshop-d16b9.appspot.com",
+      messagingSenderId: "37496788897"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+      apiKey: "AIzaSyC-0Yn5eD97LYPocRIYh7TrcRru6zZl1Qw",
+      authDomain: "organicshop-d16b9.firebaseapp.com",
+      databaseURL: "https://organicshop-d16b9.firebaseio.com",
+      projectId: "organicshop-d16b9",
+      storageBucket: "organicshop-d16b9.appspot.com",
+      messagingSenderId: "37496788897"
+  }
 };
 
 /*
